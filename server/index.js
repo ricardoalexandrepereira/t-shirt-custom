74421b6cff4468c57cfd11021a9b7f7ecf1f984e
Ricardo Pereira <ricardo_certe@hotmail.com>
2023-07-21 18:01:28 +0100

    env

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors());
-app.use(express.json({limig:"50mg"}))
+app.use(express.json({limit:"50mg"}))
 app.use("/api/v1/dalle", dalleRoutes)
 
 app.get("/", (req, res)=>{
