f09c12adc3358f363d6cf36339e5b29c0489b1ff
Ricardo Pereira <ricardo_certe@hotmail.com>
2023-07-21 18:43:19 +0100

    color change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         {snap.intro && (
             <motion.section className='home' {...slideAnimation("left")}>
                 <motion.header {...slideAnimation("down")}>
-                    <img style={{width:"40px",}} className='w-20' src={threejs} alt="logo" />
+                    <img style={{width:"70px",}} className='w-20 h-20' src={threejs} alt="logo" />
                 </motion.header>
 
                 <motion.div className='home-content' {...headContainerAnimation}>
@@ -25,7 +25,7 @@ const Home = () => {
                     </motion.div>
                     <motion.div className='flex flex-col gap-5' {...headContentAnimation}>
                         
-                        <p className='max-w-md font-normal text-gray-600 text-base'>Create your unique and exclusive shirt with our brand-new 3D Customization tool. <strong>Unleash your imagination</strong>and define your own style.</p>
+                        <p className='max-w-md font-normal text-gray-600 text-base'>Create your unique and exclusive shirt with our brand-new 3D Customization tool. <strong>Unleash your imagination</strong> and define your own style with <span className='text-base text-blue-600 font-bold'>blue cow.</span></p>
                         
                         <CustomButton type="filled" title="Customize It" handleClick={()=> state.intro = false} customStyles="w-fit px-4 py-2.5 bg-yellow-400 font-bold text-sm"/>
                     
